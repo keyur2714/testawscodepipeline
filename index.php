c54ef87329e746079841583e00a0d7e30b8cbe18
keyur2714 <keyurjava27@gmail.com>
2022-12-16 08:32:34 +0530

    Updated index.php

--- index.php
+++ index.php
@@ -1,2 +1,10 @@
 <h1>Hello Good Morning...!Radhe Krishna....!</h1>
 <p>Hi.....</p>
+<ul>
+	<li>Git</li>
+	<li>Maven/Gradle</li>
+	<li>Npm/Yarn</li>
+	<li>Jenkins/AWS Code Build</li>
+	<li>AWS Code Pipeline</li>
+	<li>Docker</li>
+</ul>
